5006b19f709d04514b4ef89b90461ef8d62fcbb7
lamhuynhb779 <lamhuynhb779@gmail.com>
2018-06-01 21:56:10 +0700

    Update

---
+++ MazeVR/Assets/HitWall.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class HitWall : MonoBehaviour
+{
+	// vars to Flash a colour when hit 
+	public AudioSource ouchs;
+	public Color NormalColour = Color.white;
+	public Color FlashColour = Color.red;
+	public float FlashDelay = 0.025f;
+	public int TimesToFlash = 3;
+	Renderer GameMesh;
+	
+	void Start()
+	{
+
+	}
+
+	public void Flash()
+	{
+		
+		StartCoroutine(OnCollisionEnter());
+	}
+
+	private IEnumerator OnCollisionEnter()
+	{
+			ouchs.Play();
+			GameMesh = GetComponent<Renderer>();
+			var renderer = GameMesh;
+			if (renderer != null)
+			{
+				for (int i = 1; i <= TimesToFlash; i++)
+				{
+					renderer.material.color = FlashColour;
+					yield return new WaitForSeconds(FlashDelay);
+					renderer.material.color = NormalColour;
+					yield return new WaitForSeconds(FlashDelay);
+				}
+
+			}
+		
+	}
+
+}
+
---
+++ MazeVR/Assets/begin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class begin : MonoBehaviour {
+	public GameObject wall;
+	// Use this for initialization
+	void Start()
+	{
+		wall.SetActive(false);
+	}
+
+	void OnTriggerEnter()
+	{
+		wall.SetActive(true);
+	}
+}
